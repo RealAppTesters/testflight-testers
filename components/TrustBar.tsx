b04908f2e8674d881df255fe1b9587e431bb35cd
RealAppTesters <Bethainm@gmail.com>
2026-07-26 02:05:16 +0200

    Create TrustBar.tsx

---
+++ components/TrustBar.tsx
@@ -0,0 +1,22 @@
+export default function TrustBar() {
+  return (
+    <div className="trust-bar">
+      <div className="trust-item">
+        <div className="number">500+</div>
+        <div className="label">Projects tested</div>
+      </div>
+      <div className="trust-item">
+        <div className="number">20+</div>
+        <div className="label">Countries</div>
+      </div>
+      <div className="trust-item">
+        <div className="number">1000+</div>
+        <div className="label">QA reports</div>
+      </div>
+      <div className="trust-item">
+        <div className="number">24h</div>
+        <div className="label">Avg. start time</div>
+      </div>
+    </div>
+  );
+}
